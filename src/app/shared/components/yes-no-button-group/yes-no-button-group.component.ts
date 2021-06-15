f4f6967453b3df6daeaf3d6b81ff9d5c32da1dfa
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-15 17:27:17 -0300

    chore: buttons activation logic

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -9,9 +9,19 @@ export class YesNoButtonGroupComponent implements OnInit {
 
   @Input() public value: string = null;
   @Input() public label = '';
+  public options = YesNoButtonGroupOptions;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public activate(value: string): void {
+    this.value = value;
+  }
+}
+
+enum YesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no'
 }
