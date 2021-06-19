469f6bf410b69ce8cb003d9881d4e8c8220584ba
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-19 15:54:45 -0300

    chore: isolating id generation into a service to facilitate future refactoring

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import * as uuid from 'uuid';
+import { UniqueIdService } from '../../services/unique-id/unique-id';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -26,8 +26,8 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   public onChange = (value: string) => {}
   public onTouched = () => {}
 
-  constructor() {
-    this.id = `yes-no-button-group-${uuid.v1()}`;
+  constructor( uniqueIdService: UniqueIdService) {
+    this.id = uniqueIdService.generateUniqueIdWithPrefix('yes-no-button-group');
    }
 
   public writeValue(value: string): void {
