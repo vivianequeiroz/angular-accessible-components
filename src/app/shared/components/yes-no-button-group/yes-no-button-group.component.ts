297c7b5738a84883888961948dd7fde4921162ae
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-19 15:43:04 -0300

    chore: avoiding problems with duplicated ids

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -20,11 +21,14 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   // at the template the two way data binding only occurs because the input and output properties have the same name "value", in which the suffix of the output one add the "Change" word
   @Output() public valueChange = new EventEmitter<string>();
   
+  public id: string = null;
   public options = YesNoButtonGroupOptions;
   public onChange = (value: string) => {}
   public onTouched = () => {}
 
-  constructor() { }
+  constructor() {
+    this.id = uuid.v1();
+   }
 
   public writeValue(value: string): void {
     this.value = value;
